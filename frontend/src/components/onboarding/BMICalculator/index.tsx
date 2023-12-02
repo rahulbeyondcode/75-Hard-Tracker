117fabeb2375bfe405fffd6302ccc9290560636e
Rahul R <rahulelavoor123@gmail.com>
2023-12-03 02:41:02 +0530

    BMI Calculator form elements added

--- frontend/src/components/onboarding/BMICalculator/index.tsx
+++ frontend/src/components/onboarding/BMICalculator/index.tsx
@@ -1,7 +1,33 @@
 const BMICalculator = () => {
   return (
     <div>
-      <h1>BMI Calculator</h1>
+      <p>
+        Before we start the challenge let’s check your{" "}
+        <b>Body Mass Index (BMI)</b> (optional)
+      </p>
+
+      <div>
+        <label htmlFor="weight">Enter your weight (in kg)</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="height">Enter your height (in cm)</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="dob">Enter your date of birth</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="gender">Enter your gender</label>
+        <input />
+      </div>
+
+      <button>Skip</button>
+      <button>Calculate</button>
     </div>
   );
 };
