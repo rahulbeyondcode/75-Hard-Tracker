14f6f7ed79c04d677acd567d92b9494dbb5bc3ad
Rahul R <rahulelavoor123@gmail.com>
2023-12-02 21:15:41 +0530

    Changed port to 3000 and updated vite config to open browser on start

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
   resolve: {
     alias: {
       assets: "/src/assets",
