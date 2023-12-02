6636efd9bc3e0ae8d99fc02ffb3fffacdd72612e
Rahul R <rahulelavoor123@gmail.com>
2023-12-03 02:19:41 +0530

    Added basic form elements for onboarding / register page

--- frontend/src/components/onboarding/Register/index.tsx
+++ frontend/src/components/onboarding/Register/index.tsx
@@ -1,7 +1,32 @@
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
+      <p>Hi, Welcome Onboard.</p>
+      <p>
+        Great to see you decided to try out the <b>75 hard challenge</b>
+      </p>
+
+      <div>
+        <label htmlFor="name">What should we call you?</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="username">Enter a username</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="password">Enter a password</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="email">Enter your email address (optional)</label>
+        <input />
+      </div>
+
+      <button>Submit</button>
     </div>
   );
 };
