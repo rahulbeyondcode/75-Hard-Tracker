c514389b6f67e2fcdc9d3376fb3613649778c380
Rahul R <rahulelavoor123@gmail.com>
2023-12-03 02:45:05 +0530

    Body measurements page form elements added

--- frontend/src/components/onboarding/BodyMeasurementsCollector/index.tsx
+++ frontend/src/components/onboarding/BodyMeasurementsCollector/index.tsx
@@ -1,7 +1,38 @@
 const BodyMeasurementsCollector = () => {
   return (
     <div>
-      <h1>Body Measurements Collector</h1>
+      <p>Lastly let’s go through your body measurements (optional)</p>
+
+      <div>
+        <label htmlFor="hip">Enter hip size (in cm)</label>
+        <input />
+        <label htmlFor="waist">Enter waist size (in cm)</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="chest">Enter chest size (in cm)</label>
+        <input />
+        <label htmlFor="arm">Enter arm size (in cm)</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="thighs">Enter thighs size (in cm)</label>
+        <input />
+        <label htmlFor="neck">Enter neck size (in cm)</label>
+        <input />
+      </div>
+
+      <div>
+        <label htmlFor="weight">Enter your weight (in kg)</label>
+        <input />
+        <label htmlFor="height">Enter your height (in cm)</label>
+        <input />
+      </div>
+
+      <button>Skip</button>
+      <button>Calculate</button>
     </div>
   );
 };
